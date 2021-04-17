5d7e7ee95f35f00a81e6a89e149a1e3d628f6781
Nicholas <Lyit2017@163.com>
2021-04-17 10:13:27 +0800

    Initial commit

---
+++ src/main/java/com/current/highcurrency/HighcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.current.highcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighcurrencyApplication.class, args);
+    }
+
+}
